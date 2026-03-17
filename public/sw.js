c1980661c803dd81e01ea4c5e7e9bee0b015c0c2
Tsubasa Narita <narita.tsubasa@gmail.com>
2026-03-17 09:14:25 +0900

    fix: ニューシャトル画像を2020系デザインに修正

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for だ〜れだ？パズルラリー
-const CACHE_NAME = 'puzzle-rally-v16';
+const CACHE_NAME = 'puzzle-rally-v17';
 
 self.addEventListener('install', (event) => {
     // 新しいSWを即座にアクティブにする
